4e32c3394b4c0698262332acda3feec13306ff65
Hamza MOTASSIM <motassimhamza99@gmail.com>
2024-01-07 20:19:58 +0100

    firebase

--- dbs/firebase/db.py
+++ dbs/firebase/db.py
@@ -7,7 +7,51 @@ firebaseURL = "https://rtdbpc-default-rtdb.firebaseio.com/"
 
 firebase_admin.initialize_app(cred, {"databaseURL": firebaseURL})
 
-# ref = db.reference("/zone_1")
-print(db.reference("/").get())
-# db.reference("/zone_1").set(8)
-# print(db.reference("/zone_1").get())
+db.reference("/Y").set(55)
+db.reference("/X").set(8)
+print(db.reference("/Y").get())
+
+db.reference("/").child('users').push({
+    'name': 'John Doe',
+    'age': 25,
+    'email': 'john@example.com'
+})
+
+
+
+# Test 1 -------------------------------
+
+# for i in range(15):
+#     db.reference("/X").set(i)
+#     print(db.reference("/zone_1").get())
+    # os.system("cls")
+# db.reference("/Y").set(55)
+# db.reference("/X").set(8)
+# print(db.reference("/Y").get())
+
+# db.reference("/").child('users').push({
+#     'name': 'John Doe',
+#     'age': 25,
+#     'email': 'john@example.com'
+# })
+
+# Test 2 -------------------------------    
+
+# import pyautogui
+# import time
+
+# def track_cursor():
+#     try:
+#         while True:
+#             x, y = pyautogui.position()
+#             db.reference("/X").set(x)
+#             db.reference("/Y").set(y)
+#             print(f"Cursor position: x={x}, y={y}")
+#             # time.sleep(1)  
+
+#     except KeyboardInterrupt:
+#         print("Tracking stopped.")
+
+# if __name__ == "__main__":
+#     track_cursor()
+
